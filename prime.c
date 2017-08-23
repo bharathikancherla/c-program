a1d9d16a07f7f4ab55d6f997ee5ea6817663934a
amith sharma <amithsharmaraj@gmail.com>
2017-08-23 12:31:08 +0530

    even or odd program

---
+++ prime.c
@@ -0,0 +1,16 @@
+//this is a program to check whether the given number is even or odd
+#include<stdio.h>
+void main()
+{
+	int i;
+	printf("enter a number");
+	scanf("%d",&i);
+	if(i%2==0)
+	{
+		printf("the given number is even number");
+	}
+	else
+	{
+		printf("the given number is odd number");
+	}
+}
